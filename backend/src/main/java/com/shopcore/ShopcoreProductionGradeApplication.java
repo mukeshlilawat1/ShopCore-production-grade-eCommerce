55e02bab15312225ea69e8b01c69edb4d2077cdc
Mukesh Lilawat <mukeshkumarlilawat1@gmail.com>
2026-02-11 16:51:53 +0530

    backend setup

---
+++ backend/src/main/java/com/shopcore/ShopcoreProductionGradeApplication.java
@@ -0,0 +1,13 @@
+package com.shopcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcoreProductionGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopcoreProductionGradeApplication.class, args);
+	}
+
+}
